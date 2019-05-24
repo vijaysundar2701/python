999275cc5aa6d338be57bc9cba09d62d3883cd67
vijaysundar2701 <42805691+vijaysundar2701@users.noreply.github.com>
2019-05-24 15:37:20 +0530

    Create largenumbers.py

---
+++ largenumbers.py
@@ -0,0 +1,9 @@
+v=int(input("enter the value of v"))
+i=int(input("enter the value of i"))
+j=int(input("enter the value of j"))
+if(v>i and v>j):
+    print(v)
+elif(i>j):
+    print(i)
+else:
+    print(j)
