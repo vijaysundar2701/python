821ba4d32f580da691ea34ba434c652b1c62395f
vijaysundar2701 <42805691+vijaysundar2701@users.noreply.github.com>
2019-06-11 21:21:52 +0530

    Update and rename sumofnk.py to NKsum.py

---
+++ NKsum.py
@@ -0,0 +1,10 @@
+lst=[]
+N=int(input())
+K=int(input())
+s=1
+for i in range(0,N):
+    ele=int(input())
+    N=lst.append(ele)
+for N in range(2,K+1):
+    s=s+N
+print(s)
--- sumofnk.py
+++
@@ -1,6 +0,0 @@
-N=int(input())
-K=int(input())
-sum=1
-for N in range(2,K+1):
-    sum=sum+N
-print(sum)
