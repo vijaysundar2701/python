0079fed73cdfd330a3555ccc07b377f239203910
vijaysundar2701 <42805691+vijaysundar2701@users.noreply.github.com>
2019-06-24 20:38:18 +0530

    Create sameindexno.py

---
+++ sameindexno.py
@@ -0,0 +1,10 @@
+p=int(input())
+q=list(map(int,input().split()))
+r=[]
+for i in range(0,len(q)):
+    if i==q[i]:
+        r.append(q[i])
+if len(r)>0:
+    print(*r,sep='')
+else:
+    print("-1")
