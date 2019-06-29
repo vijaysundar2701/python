a05128ff7602b3f19616f6770e039de02980c2e3
vijaysundar2701 <42805691+vijaysundar2701@users.noreply.github.com>
2019-06-29 18:26:07 +0530

    Update pro16.py

--- pro16.py
+++ pro16.py
@@ -1,11 +1,11 @@
-sh=int(input())
-se=list(map(int,input().split()))
-x=[1]*sh
-for i in range(sh):
+ab=int(input())
+cd=list(map(int,input().split()))
+xy=[1]*ab
+for i in range(ab):
     if i==0:
-        if se[i]>se[i+1]:
-            x[i]=x[i]+x[i+1]
+        if cd[i]>cd[i+1]:
+            xy[i]=xy[i]+xy[i+1]
     elif i>0:
-        if se[i]>se[i-1]:
-            x[i]=x[i]+x[i-1]
-print(sum(x))
+        if cd[i]>cd[i-1]:
+            xy[i]=xy[i]+xy[i-1]
+print(sum(xy))
