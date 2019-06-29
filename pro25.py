c8b89a45b71e04d595c33e18fa66592e56b93d80
vijaysundar2701 <42805691+vijaysundar2701@users.noreply.github.com>
2019-06-29 19:29:49 +0530

    Update pro25.py

--- pro25.py
+++ pro25.py
@@ -1,12 +1,12 @@
-t=int(input())
+q=int(input())
 l=list(map(int,input().split()))
-s=[]
-a=1
-for i in range(t-1):
+ss=[]
+v=1
+for i in range(q-1):
     if l[i]<l[i+1]:
-        a+=1
+        v+=1
     else:
-        s.append(a)
-        a=1
-s.append(a)
-print(max(s))
+        ss.append(v)
+        v=1
+ss.append(v)
+print(max(ss))
