becd3ce13b96297630a11982e7f3ef1a451ad6f7
vijaysundar2701 <42805691+vijaysundar2701@users.noreply.github.com>
2019-08-14 18:07:02 +0530

    Update beg56.py

--- beg56.py
+++ beg56.py
@@ -1,5 +1,14 @@
 h=input()
-if h.isalnum():
-    print("yes")
+c=0
+k=0
+for i in h:
+    if(i.isalpha()):
+        c=c+1
+    elif(i.isdigit()):
+        k=k+1
+    else:
+        d=0
+if c!=0 and k!=0:
+    print("Yes")
 else:
-    print("no")
+    print("No")
